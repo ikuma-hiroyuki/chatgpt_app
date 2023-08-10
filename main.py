dcf2f097286eb59341c923e5c94126789fc2fa7e
ikuma-hiroyuki <chemicalbondo@gmail.com>
2023-08-11 07:13:04 +0900

    メソッド名の変ˆ更

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if is_chat_history_open() and excel_path.exists():
     print(f"{excel_path.name} が開かれています。閉じてから再度実行してください。")
 else:
     gpt = ChatGPT()
-    gpt.start_chat()
+    gpt.chat_start()
     output_excel(gpt)
