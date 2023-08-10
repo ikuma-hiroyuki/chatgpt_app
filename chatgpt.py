cc226559c94f3c6ba34995b7e0919d5223f477c8
ikuma-hiroyuki <chemicalbondo@gmail.com>
2023-08-11 07:14:33 +0900

    メソッド名の変更、関心の分離、exit()処理の一元化 など複数箇所のリファクタリングを行った ˆ

--- chatgpt.py
+++ chatgpt.py
@@ -9,69 +9,73 @@ openai.api_key = os.getenv("API_KEY")
 
 
 class ChatGPT:
-    exit_command: str = "exit()"
-    default_model: str = "gpt-3.5-turbo"
+    EXIT_COMMAND: str = "exit()"
+    DEFAULT_MODEL: str = "gpt-3.5-turbo"
 
     def __init__(self, summary_length: int = 10):
+        self.gpt_model = ""
         self.chat_history: list[dict] = []
         self.chat_summary: str = ""
         self._initial_prompt: str = ""
         self._summary_length: int = summary_length
 
     @staticmethod
-    def _print_api_error_message(e):
-        """APIエラーのメッセージを表示する"""
+    def _print_error_message(message):
+        """エラーメッセージを表示する"""
+        print(f"{Fore.RED}{message}{Fore.RESET}")
+
+    @staticmethod
+    def _throw_api_errors(e):
+        """APIエラーを振り分ける"""
 
         e = type(e)  # type(e)とすることで、eのサブクラスの型を取得できる
         if isinstance(e, (openai.error.APIError, openai.error.ServiceUnavailableError)):
-            print(f"{Fore.RED}OpenAI側でエラーが発生しています。少し待ってから再度試してください。{Fore.RESET}")
+            ChatGPT._print_error_message("OpenAI側でエラーが発生しています。少し待ってから再度試してください。")
             print("サービス稼働状況は https://status.openai.com/ で確認できます。")
-        elif e == openai.error.RateLimitError:
-            print(f"{Fore.RED}ネットワークに問題があります。設定を見直すか少し待ってから再度試してください。{Fore.RESET}")
-        elif e == openai.error.AuthenticationError:
-            print(f"{Fore.RED}APIキーまたはトークンが無効もしくは期限切れです。{Fore.RESET}")
+        elif isinstance(e, openai.error.RateLimitError):
+            ChatGPT._print_error_message("ネットワークに問題があります。設定を見直すか少し待ってから再度試してください。")
+        elif isinstance(e, openai.error.AuthenticationError):
+            ChatGPT._print_error_message("APIキーまたはトークンが無効もしくは期限切れです。")
 
-    def fetch_gpt_model_list(self) -> list[str]:
+    @staticmethod
+    def fetch_gpt_model_list() -> list[str] | None:
         """
-        GPTモデルの一覧を取得
-
-        エラーが発生した場合はプログラムを終了させる
-        :return: GPTモデルの一覧
+        GPTモデルの一覧を取得する。APIエラーでも出るが取得できないときはNoneを返す。
+        :return: GPTモデルの一覧もしくはNone
         """
 
-        # GPTモデルの一覧を取得
         try:
             model_list = openai.Model.list()
         except openai.error.OpenAIError as e:
-            self._print_api_error_message(e)
-            exit()
+            ChatGPT._throw_api_errors(e)
+            return None
         else:
             models = [model.id for model in model_list.data if "gpt" in model.id]
             models.sort()
             return models
 
-    def _choice_chat_model(self) -> str:
+    def _choice_chat_model(self) -> str | None:
         """
-        GPTモデルの一覧を選択させる
-
-        :return: 選択されたモデルの名称
+        GPTモデルの一覧を選択させる。APIエラーでモデルが取得できないときはNoneを返す。
+        :return: 選択されたモデルの名称もしくはNone
         """
 
         models_list = self.fetch_gpt_model_list()
+        if not models_list:
+            return None
 
-        # モデルの選択
         while True:
             # モデルの一覧を表示
+            print("\nAIとのチャットに使用するモデルの番号を入力しEnterキーを押してしてください。")
             for i, model in enumerate(models_list):
                 print(f"{i}: {model}")
+            selected_model = input(f"何も入力しない場合は{Fore.GREEN} {self.DEFAULT_MODEL} {Fore.RESET}が使われます。: ")
 
-            selected_model = input("使用するモデル番号を入力しEnterキーを押してしてください。"
-                                   f"何も入力しない場合は '{self.default_model}' が使われます。: ")
             # 何も入力されなかったとき
             if not selected_model:
-                return self.default_model
+                return self.DEFAULT_MODEL
             # 数字以外が入力されたとき
-            elif not selected_model.isdigit():
+            if not selected_model.isdigit():
                 print(f"{Fore.RED}数字を入力してください。{Fore.RESET}")
             # 選択肢に存在しない番号が入力されたとき
             elif not int(selected_model) in range(len(models_list)):
@@ -83,90 +87,92 @@ class ChatGPT:
     def _input_user_prompt(self) -> str:
         """
         ユーザーからの入力を受け付ける
-
-        一番最初のプロンプトで終了コマンドが入力された場合は、チャットを終了する。
-        2回目以降のプロンプトで終了コマンドが入力された場合は、空文字を返す。
-        それ以外の場合は、ユーザーの入力を返す。
-        :return: ユーザーのプロンプト及び空文字
+        :return: ユーザーのプロンプト
         """
 
         while True:
-            # ユーザー入力の受付と履歴への追加
             while True:
-                user_prompt = input(f"\n{Fore.CYAN}あなた:{Fore.RESET} ")
+                user_prompt = input(f"{Fore.CYAN}あなた:{Fore.RESET} ")
                 if not user_prompt:
                     print(f"{Fore.YELLOW}プロンプトを入力してください。{Fore.RESET}")
                 else:
                     break
 
             if not self._initial_prompt:
-                # いきなり終了コマンドが入力されたときはプログラム終了
-                if user_prompt == self.exit_command:
-                    exit()
                 self._initial_prompt = user_prompt
 
-            if user_prompt == self.exit_command:
-                return ""
-            else:
-                return user_prompt
+            return user_prompt
 
-    def start_chat(self):
-        """
-        AIアシスタントとユーザーとのチャットを開始し、チャットが終了したら要約を作成する。
-
-        ユーザーからの入力を受け取り、AIアシスタントが応答を生成する。
-        ユーザーが exit() と入力すると、チャットは終了する。
-        """
-
-        model = self._choice_chat_model()
-
-        print(f"\nAIアシスタントとチャットを始めます。チャットを終了するには {self.exit_command} と入力してください。")
+    def _give_role_to_gpt(self):
+        """ AIアシスタントに与える役割を入力させる """
+        print(f"AIアシスタントとチャットを始めます。")
         system_content = input("AIアシスタントに与える役割がある場合は入力してください。"
                                "ない場合はそのままEnterキーを押してください。: ")
-
         if system_content:
             self.chat_history.append({"role": "system", "content": system_content})
 
-        while True:
-            # ユーザーのプロンプトを受け取り履歴に追加する
-            user_prompt = self._input_user_prompt()
-            if not user_prompt:
-                # ユーザーが終了コマンドを入力した場合は空文字が返されチャットを終了する
-                break
-            self.chat_history.append({"role": "user", "content": user_prompt})
+    def _fetch_gpt_answer(self):
+        """
+        GPTモデルにユーザーのプロンプトを与えて応答を生成させ、チャット履歴に追加するとともに、応答を返却する。
+        :returns: AIアシスタントの応答と役割
+        """
 
-            # GPTによる応答
-            completion = openai.ChatCompletion.create(model=model, messages=self.chat_history)
-            gpt_answer = completion.choices[0].message.content
-            gpt_role = completion.choices[0].message.role
-
-            # 応答の表示と履歴への追加
-            print(f"\n{Fore.GREEN}AIアシスタント:{Fore.RESET} {gpt_answer}")
-            self.chat_history.append({"role": gpt_role, "content": gpt_answer})
-
-        self._generate_summary()
+        completion = openai.ChatCompletion.create(model=self.gpt_model, messages=self.chat_history)
+        answer = completion.choices[0].message.content
+        role = completion.choices[0].message.role
+        self.chat_history.append({"role": role, "content": answer})
+        return answer, role
 
     def _generate_summary(self):
         """
-        チャットの履歴から要約を生成する。
+        チャットの履歴から要約を生成し返す。
 
         要約する文字数は self._summary_length で指定する。
         ただし、GPTに文字数を指定して要約を生成させると、指定した文字数よりも多くなる場合がある。
         その場合、要約の文字数を self._summary_length に合わせ、最後に ... を追加する。
+        :return: チャットの要約
         """
 
         # chat_history の先頭に要約の依頼を追加
         summary_request = {"role": "system",
                            "content": "あなたはユーザーの依頼を要約する役割を担います。"
                                       f"以下のユーザーの依頼を必ず全角{self._summary_length}文字以内で要約してください"}
-
         # GPTによる要約を取得
         messages = [summary_request, {"role": "user", "content": self._initial_prompt}]
-        completion = openai.ChatCompletion.create(model=self.default_model, messages=messages)
+        completion = openai.ChatCompletion.create(model=self.gpt_model, messages=messages)
         summary = completion.choices[0].message.content
 
         # 要約を調整
         if len(summary) > self._summary_length:
-            self.chat_summary = summary[:self._summary_length] + "..."
+            return summary[:self._summary_length] + "..."
         else:
-            self.chat_summary = summary
+            return summary
+
+    def chat_start(self):
+        """
+        AIアシスタントとユーザーとのチャットを開始し、チャットが終了したら要約を作成する。
+
+        ユーザーからの入力を受け取り、AIアシスタントが応答を生成する。
+        ユーザーが exit() と入力するとチャット終了。
+        """
+
+        self._give_role_to_gpt()
+        self.gpt_model = self._choice_chat_model()
+        if not self.gpt_model:
+            print(f"{Fore.RED}エラーが発生しました。プログラムを終了します。{Fore.RESET}")
+            exit()
+
+        print(f"\nチャットを終了するには{Fore.GREEN} {self.EXIT_COMMAND} {Fore.RESET}と入力します。")
+        user_prompt = self._input_user_prompt()
+
+        # いきなり終了コマンドが入力された場合はプログラム終了
+        if not self._initial_prompt and user_prompt == self.EXIT_COMMAND:
+            exit()
+
+        while user_prompt != self.EXIT_COMMAND:
+            self.chat_history.append({"role": "user", "content": user_prompt})
+            gpt_answer, gpt_role = self._fetch_gpt_answer()
+            print(f"\n{Fore.GREEN}AIアシスタント:{Fore.RESET} {gpt_answer}")
+            user_prompt = self._input_user_prompt()
+        else:
+            self.chat_summary = self._generate_summary()
