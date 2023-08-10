e7f273bffc47c5c973ffba624c0e1a0183623fed
ikuma <chemicalbondo@gmail.com>
2023-08-10 13:44:01 +0900

    ユーザーの入力を受け付ける処理を関数に分割し、チャット開始エントリポイントも修正

--- chatgpt.py
+++ chatgpt.py
@@ -6,12 +6,11 @@ from dotenv import load_dotenv
 
 from output_excel import is_chat_history_open, excel_path, output_excel
 
-# 初期設定
-load_dotenv()
-openai.api_key = os.getenv("API_KEY")
-
 
 class ChatGPT:
+    exit_command: str = "exit()"
+    default_model: str = "gpt-3.5-turbo"
+
     def __init__(self, summary_length: int = 10):
         self.chat_history: list[dict] = []
         self.chat_summary: str = ""
@@ -19,7 +18,7 @@ class ChatGPT:
         self.summary_length: int = summary_length
 
     @staticmethod
-    def _api_error_message(e):
+    def _print_api_error_message(e):
         """APIエラーのメッセージを表示する"""
 
         if e in [openai.error.APIError, openai.error.ServiceUnavailableError]:
@@ -34,6 +33,7 @@ class ChatGPT:
         """
         GPTモデルの一覧を取得
 
+        エラーが発生した場合はプログラムを終了させる
         :return: GPTモデルの一覧
         """
 
@@ -41,7 +41,7 @@ class ChatGPT:
         try:
             model_list = openai.Model.list()
         except openai.error.OpenAIError as e:
-            self._api_error_message(type(e))  # type(e)とすることで、eのサブクラスの型を取得できる
+            self._print_api_error_message(type(e))  # type(e)とすることで、eのサブクラスの型を取得できる
             exit()
         else:
             models = [model.id for model in model_list.data if "gpt" in model.id]
@@ -55,7 +55,6 @@ class ChatGPT:
         :return: 選択されたモデルの名称
         """
 
-        default_model = "gpt-3.5-turbo"
         models_list = self.fetch_gpt_model_list()
 
         # モデルの選択
@@ -65,10 +64,10 @@ class ChatGPT:
                 print(f"{i}: {model}")
 
             selected_model = input("使用するモデル番号を入力しEnterキーを押してしてください。"
-                                   f"何も入力しない場合は '{default_model}' が使われます。: ")
+                                   f"何も入力しない場合は '{self.default_model}' が使われます。: ")
             # 何も入力されなかったとき
             if not selected_model:
-                return default_model
+                return self.default_model
             # 数字以外が入力されたとき
             elif not selected_model.isdigit():
                 print(f"{Fore.RED}数字を入力してください。{Fore.RESET}")
@@ -79,43 +78,60 @@ class ChatGPT:
             elif int(selected_model) in range(len(models_list)):
                 return models_list[int(selected_model)]
 
-    def _start_chat(self):
+    def _input_user_prompt(self) -> str:
         """
-        AIアシスタントとユーザーとのチャットを開始し、チャットが終了したら要約を作成する。
+        ユーザーからの入力を受け付ける
 
-        ユーザーからの入力を受け取り、AIアシスタントが応答を生成します。
-        ユーザーが 'exit()'と入力すると、チャットは終了します。
+        一番最初のプロンプトで終了コマンドが入力された場合は、チャットを終了する。
+        2回目以降のプロンプトで終了コマンドが入力された場合は、空文字を返す。
+        それ以外の場合は、ユーザーの入力を返す。
+        :return: ユーザーのプロンプト及び空文字
         """
-
-        exit_command = "exit()"
-        model = self._choice_chat_model()
-
-        print(f"\nAIアシスタントとチャットを始めます。チャットを終了するには {exit_command} と入力してください。")
-        system_content = input("AIアシスタントに演じて欲しい役割がある場合は入力してください。"
-                               "ない場合はそのままEnterキーを押してください。: ")
-
-        # チャットを開始
-        if system_content:
-            self.chat_history.append({"role": "system", "content": system_content})
 
         while True:
             # ユーザー入力の受付と履歴への追加
             while True:
-                user_request = input(f"\n{Fore.CYAN}あなた:{Fore.RESET} ")
-                if not user_request:
+                user_prompt = input(f"\n{Fore.CYAN}あなた:{Fore.RESET} ")
+                if not user_prompt:
                     print(f"{Fore.YELLOW}プロンプトを入力してください。{Fore.RESET}")
                 else:
                     break
 
             if not self.initial_prompt:
-                self.initial_prompt = user_request
+                # いきなり終了コマンドが入力されたときはプログラム終了
+                if user_prompt == self.exit_command:
+                    exit()
+                self.initial_prompt = user_prompt
 
-            if self.initial_prompt and user_request == exit_command:
+            if user_prompt == self.exit_command:
+                return ""
+            else:
+                return user_prompt
+
+    def _start_chat(self):
+        """
+        AIアシスタントとユーザーとのチャットを開始し、チャットが終了したら要約を作成する。
+
+        ユーザーからの入力を受け取り、AIアシスタントが応答を生成する。
+        ユーザーが exit() と入力すると、チャットは終了する。
+        """
+
+        model = self._choice_chat_model()
+
+        print(f"\nAIアシスタントとチャットを始めます。チャットを終了するには {self.exit_command} と入力してください。")
+        system_content = input("AIアシスタントに与える役割がある場合は入力してください。"
+                               "ない場合はそのままEnterキーを押してください。: ")
+
+        if system_content:
+            self.chat_history.append({"role": "system", "content": system_content})
+
+        while True:
+            # ユーザーのプロンプトを受け取り履歴に追加する
+            user_prompt = self._input_user_prompt()
+            if not user_prompt:
+                # ユーザーが終了コマンドを入力した場合は空文字が返されチャットを終了する
                 break
-            elif user_request == exit_command:
-                exit()
-
-            self.chat_history.append({"role": "user", "content": user_request})
+            self.chat_history.append({"role": "user", "content": user_prompt})
 
             # GPTによる応答
             completion = openai.ChatCompletion.create(model=model, messages=self.chat_history)
@@ -133,25 +149,30 @@ class ChatGPT:
 
         # chat_history の先頭に要約の依頼を追加
         summary_request = {"role": "system",
-                           "content": f"あなたはユーザーの依頼を要約する役割を担います。以下のユーザーの依頼を必ず全角{self.summary_length}文字以内で要約してください"}
+                           "content": "あなたはユーザーの依頼を要約する役割を担います。"
+                                      f"以下のユーザーの依頼を必ず全角{self.summary_length}文字以内で要約してください"}
 
         # GPTによる要約を取得
         messages = [summary_request, {"role": "user", "content": self.initial_prompt}]
-        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-        self.chat_summary = completion.choices[0].message.content
+        completion = openai.ChatCompletion.create(model=self.default_model, messages=messages)
+        summary = completion.choices[0].message.content
 
         # 要約を調整
-        if len(self.chat_summary) > self.summary_length:
-            self.chat_summary = self.chat_summary[:self.summary_length] + "..."
+        if len(summary) > self.summary_length:
+            self.chat_summary = summary[:self.summary_length] + "..."
 
     def run(self):
-        if is_chat_history_open():
+        """ チャットを開始するエントリポイント。"""
+        if is_chat_history_open() and excel_path.exists():
             print(f"{excel_path.name} が開かれています。閉じてから再度実行してください。")
         else:
             self._start_chat()
-            output_excel(self)
 
 
 if __name__ == "__main__":
+    load_dotenv()
+    openai.api_key = os.getenv("API_KEY")
+
     chat = ChatGPT()
     chat.run()
+    output_excel(chat)
